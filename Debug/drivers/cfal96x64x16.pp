4db2c8b69292bfad21df9b588f56777a2c0cc743
yyuuabdb <yyuuabdb@gmail.com>
2026-03-31 15:52:34 +0900

    Add files via upload

---
+++ Debug/drivers/cfal96x64x16.pp
@@ -0,0 +1,33 @@
+# FIXED
+
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/examples/boards/dk-tm4c123g/drivers/cfal96x64x16.c
+drivers/cfal96x64x16.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h
+drivers/cfal96x64x16.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h
+drivers/cfal96x64x16.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+drivers/cfal96x64x16.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+drivers/cfal96x64x16.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
+drivers/cfal96x64x16.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/ssi.h
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/rom.h
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/pin_map.h
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/grlib/grlib.h
+drivers/cfal96x64x16.obj: C:/ti/TivaWare_C_Series-1.1/examples/boards/dk-tm4c123g/drivers/cfal96x64x16.h
+
+C:/ti/TivaWare_C_Series-1.1/examples/boards/dk-tm4c123g/drivers/cfal96x64x16.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/ssi.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-1.1/grlib/grlib.h: 
+C:/ti/TivaWare_C_Series-1.1/examples/boards/dk-tm4c123g/drivers/cfal96x64x16.h: 
