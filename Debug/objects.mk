7d1dac0372da5c201bbe4779d7cca713e85ca3a5
yyuuabdb <yyuuabdb@gmail.com>
2026-03-31 15:51:39 +0900

    Add files via upload

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"libc.a" -l"C:/ti/TivaWare_C_Series-1.1/examples/boards/dk-tm4c123g/hello/ccs/../../../../../driverlib/ccs/Debug/driverlib.lib" -l"C:/ti/TivaWare_C_Series-1.1/examples/boards/dk-tm4c123g/hello/ccs/../../../../../grlib/ccs/Debug/grlib.lib"
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../hello_ccs.cmd 
+
+ASM_SRCS += \
+../switch.asm 
+
+C_SRCS += \
+../hello.c \
+../startup_ccs.c 
+
+OBJS += \
+./hello.obj \
+./startup_ccs.obj \
+./switch.obj 
+
+ASM_DEPS += \
+./switch.pp 
+
+C_DEPS += \
+./hello.pp \
+./startup_ccs.pp 
+
+C_DEPS__QUOTED += \
+"hello.pp" \
+"startup_ccs.pp" 
+
+OBJS__QUOTED += \
+"hello.obj" \
+"startup_ccs.obj" \
+"switch.obj" 
+
+ASM_DEPS__QUOTED += \
+"switch.pp" 
+
+C_SRCS__QUOTED += \
+"../hello.c" \
+"../startup_ccs.c" 
+
+ASM_SRCS__QUOTED += \
+"../switch.asm" 
+
+
